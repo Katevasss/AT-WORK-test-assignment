d6cb87650da31f010c6461c5b7057ce8c6de6c3a
katevas <katya.vasilevskaya.01@mail.ru>
2024-04-10 13:32:08 +0300

    initial commit

---
+++ src/pages/EditProfilePage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
+import styles from "./EditProfilePage.module.scss";
+
+const EditProfilePage = () => {
+  return (
+    <>
+      <h2>редактированиет</h2>
+    </>
+  );
+};
+
+export default EditProfilePage;
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
+import styles from "./MainPage.module.scss";
+
+const MainPage = () => {
+  return (
+    <>
+      <h2>Вход в личный кабинет</h2>
+    </>
+  );
+};
+
+export default MainPage;
